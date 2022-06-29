2a6d579698e11f6d7b00468c7bcd1c3e709628ce
emonShimoul <emonshimoul@gmail.com>
2022-06-29 15:04:23 +0600

    added GET api to display all data and also a single specific data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
 
@@ -20,6 +21,22 @@ async function run() {
         await client.connect();
         const database = client.db('carMechanics');
         const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async(req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // GET Single Service
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log('getting...', id);
+            const query = { _id:ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
 
         // POST API
         app.post('/services', async(req, res) => {
